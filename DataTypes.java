625c2dbeb54c0283dad5ccf05d65db17aabb7868
Hariharan <hariharan9572@gmail.com>
2025-12-10 02:59:13 +0530

    day 2

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main (String[] args){
+        int num = 7;
+        float py = 3.14f;
+        double py1 = 3.149876;
+        char alaphabet = 'a';
+        boolean isTrue = true;
+
+        System.out.println(num);
+        System.out.println(py);
+        System.out.println(py1);
+        System.out.println(alaphabet);
+        System.out.println(isTrue);
+    }
+}
